c4c2c42e46d368e51eb17aa213c76778f9585b8d
Angelo Geulin <angelogeulin123@gmail.com>
2023-12-11 16:38:36 +0800

    chore: tidy up boilerplate code

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native"
+
+export default function HomePage() {
+  return (
+    <View
+      style={{
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+      }}
+    >
+      <Text>Hello, world!</Text>
+    </View>
+  )
+}
